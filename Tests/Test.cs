981183e051911ddc8f8bd84783deae005567c1a6
Vitalijus42 <vitalijus.gamperis@gmail.com>
2023-01-12 23:34:09 +0200

    Add project files.

---
+++ Tests/Test.cs
@@ -0,0 +1,21 @@
+﻿using Framework;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class Test
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Driver.CreateDriver();
+            Driver.OpenPage("https://www.autobusubilietai.lt");
+        
+        }
+    }
+}
