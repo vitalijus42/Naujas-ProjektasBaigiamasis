ab9e3b2a62855f1a14ed65e7e6c9f2f848af62d8
Vitalijus42 <vitalijus.gamperis@gmail.com>
2023-01-16 05:35:20 +0200

    ikeliau kalbu testa. negali atlikti clicko Seleniumas

---
+++ Framework/POM/languagesDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.POM
+{
+    public class languagesDemo
+    {
+        private static string url = "https://autobusubilietai.lt";
+        private static string allowCookiesButton = "//*[@aria-label='allow cookies']";
+        private static string languageLocatorButton = "//*[@class='language-dropdown dropdown ']/div";
+        public static string EnglishLanguageLocator = "en";
+        private static string DropdownHeaderButtonLocator = "header-en";
+
+        public static void ClickEnglishLanguageButton()
+        {
+            Common.WaitForElementToBeClickable(EnglishLanguageLocator);
+            Common.WaitAndClick(EnglishLanguageLocator);
+        }
+
+        public static void HoverTheMouseOver(string text)
+        {
+            Common.MoveMousePauseAndClick(languageLocatorButton, text);
+        }
+
+        public static void Open()
+        {
+            Driver.OpenPage(url);
+            Common.WaitAndClick(allowCookiesButton);
+        }
+
+        public static string GetTheSelectedLanguage()
+        {
+            return Common.GetElementText(DropdownHeaderButtonLocator);
+        }
+    }
+}
---
+++ Tests/languagesDemoTest.cs
@@ -0,0 +1,46 @@
+﻿using Framework.POM;
+using Framework;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.AccessControl;
+using System.Net.NetworkInformation;
+
+namespace Tests
+{
+    internal class languagesDemotests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.CreateDriver();
+            languagesDemo.Open();
+        }
+        [Test]
+        public void ChangeLanguageDropdown()
+        {
+
+            string ExpectedLanguage = "EN";
+            languagesDemo.HoverTheMouseOver("\"//*[@class='language-dropdown dropdown ']/div\"");
+            languagesDemo.ClickEnglishLanguageButton();
+            string actualLanguage = languagesDemo.GetTheSelectedLanguage();
+
+            Assert.AreEqual(ExpectedLanguage, actualLanguage);  
+
+
+        }
+        [TearDown]
+        public void teardown()
+        {
+            Driver.CloseDriver();
+        }
+
+
+
+
+    }
+}
+
